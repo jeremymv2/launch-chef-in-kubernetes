7f7d71bb41a07f02e908ccee567e820dacf3b48e
Jeremy J. Miller <jm@chef.io>
2018-01-26 12:43:36 -0600

    machine type change
    Signed-off-by: Jeremy J. Miller <jm@chef.io>

--- cluster-up.sh
+++ cluster-up.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gcloud container clusters create chef-server-cluster --num-nodes=3 --machine-type n1-standard-16
+gcloud container clusters create chef-server-cluster --num-nodes=3 --machine-type n1-standard-8
 gcloud container clusters get-credentials chef-server-cluster
 kubectl cluster-info
 
